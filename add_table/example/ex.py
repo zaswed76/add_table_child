f02d040c9924f6997e556fabe46c6f1758d9415c
serg <zaswed76@gmail.com>
2017-11-11 21:40:36 +0200

    add structure

--- add_table/example/ex.py
+++ add_table/example/ex.py
@@ -1,8 +1,17 @@
 
-from typing import List
 
-odd: List[int] = []
 
-odd.append(1)
-odd.append("a")
-print(odd)
+gr = {
+    "2": 35,
+    "1": 25,
+    "3": 45
+}
+
+def calc_rang(time):
+    print(gr.items())
+    for k, v in gr.items():
+        if time <= v:
+            return k
+    else: return ""
+
+print(calc_rang(20))
